e7c3e30ea1d147ca5f45f4626f25a10a407fadb8
Amir <haghighi251@gmail.com>
2024-07-25 20:32:45 +0200

    feat: add GetUserDetailsUseCase in invis context

--- src/contexts/invia/application/usecases/GetUserDetailsUseCase.spec.ts
+++ src/contexts/invia/application/usecases/GetUserDetailsUseCase.spec.ts
@@ -4,15 +4,17 @@ import { SERVICE_IDENTIFIER } from '@/contexts/invia/infrastructure/inversify/SE
 import { UserRepository } from '@/contexts/invia/domain/services/UserRepository';
 import { User } from '@/shared/types/invis/UserSchema';
 import { err, ok, Result } from 'neverthrow';
-import GetUserListUseCase from './GetUsersUseCase';
+
 import { invisMockedUsers } from '@/__mocks__/invis/users';
+import GetUserDetailsUseCase from './GetUserDetailsUseCase';
 
 jest.mock('@/contexts/invia/domain/services/UserRepository');
 
 describe('GetUserListUseCase', () => {
   let container: Container;
-  let getUserListUseCase: GetUserListUseCase;
+  let getUserDetailsUseCase: GetUserDetailsUseCase;
   let userRepository: jest.Mocked<UserRepository>;
+  const userId = 1;
 
   beforeEach(() => {
     container = new Container();
@@ -20,35 +22,37 @@ describe('GetUserListUseCase', () => {
     userRepository = new UserRepository() as jest.Mocked<UserRepository>;
     container.bind<UserRepository>(SERVICE_IDENTIFIER.UserRepository).toConstantValue(userRepository);
 
-    getUserListUseCase = new GetUserListUseCase(userRepository);
+    getUserDetailsUseCase = new GetUserDetailsUseCase(userRepository);
   });
 
-  it('should return a list of users on success', async () => {
-    userRepository.findAll.mockResolvedValue(ok(invisMockedUsers));
+  it('should return a user on success', async () => {
+    userRepository.findUserById.mockResolvedValue(ok(invisMockedUsers[0]));
 
-    const result = await getUserListUseCase.execute();
+    const result = await getUserDetailsUseCase.execute(userId);
 
-    expect(result).toEqual(invisMockedUsers);
-    expect(userRepository.findAll).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(invisMockedUsers[0]);
+    expect(userRepository.findUserById).toHaveBeenCalledTimes(1);
+    expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
   });
 
-  it('should throw an error when UserRepository.findAll fails', async () => {
+  it('should throw an error when UserRepository.findUserById fails', async () => {
     const mockError = 'Test error';
-    userRepository.findAll.mockResolvedValue(err(mockError));
+    userRepository.findUserById.mockResolvedValue(err(mockError));
 
-    await expect(getUserListUseCase.execute()).rejects.toThrow(mockError);
-    expect(userRepository.findAll).toHaveBeenCalledTimes(1);
+    await expect(getUserDetailsUseCase.execute(userId)).rejects.toThrow(mockError);
+    expect(userRepository.findUserById).toHaveBeenCalledTimes(1);
+    expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
   });
 
   it('should log the error when UserRepository.findAll fails', async () => {
     const mockError = 'Test error';
     const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
 
-    userRepository.findAll.mockResolvedValue(err(mockError));
+    userRepository.findUserById.mockResolvedValue(err(mockError));
 
-    await expect(getUserListUseCase.execute()).rejects.toThrow(mockError);
+    await expect(getUserDetailsUseCase.execute(userId)).rejects.toThrow(mockError);
 
-    expect(consoleErrorSpy).toHaveBeenCalledWith('There is a domain error in getting users list.');
+    expect(consoleErrorSpy).toHaveBeenCalledWith('There is a domain error in getting user details.');
     expect(consoleErrorSpy).toHaveBeenCalledWith('We should convert it to a proper http error and also log it.');
     expect(consoleErrorSpy).toHaveBeenCalledWith(mockError);
 
--- src/contexts/invia/application/usecases/GetUserDetailsUseCase.ts
+++ src/contexts/invia/application/usecases/GetUserDetailsUseCase.ts
@@ -4,19 +4,20 @@ import { SERVICE_IDENTIFIER } from "@/contexts/invia/infrastructure/inversify/SE
 import { UserRepository } from "@/contexts/invia/domain/services/UserRepository";
 import { Nullable } from "@/contexts/shared/domain/entities/Nullable";
 import { User } from "@/shared/types/invis/UserSchema";
+import { IGetUserDetails } from "@/contexts/invia/infrastructure/interfaces/IGetUserDetails";
 
 @injectable()
-export default class GetUserListUseCase implements IGetUserUseCase {
+export default class GetUserDetailsUseCase implements IGetUserDetails {
     constructor(@inject(SERVICE_IDENTIFIER.UserRepository) private readonly userRepository: UserRepository) {}
 
-    async execute(): Promise<Nullable<Array<User>>> {
+    async execute(userId: number): Promise<Nullable<User>> {
 
-        const result = await this.userRepository.findAll();
+        const result = await this.userRepository.findUserById(userId);
 
         if (result.isErr()) {
             const exception = result.error;
 
-            console.error(`There is a domain error in getting users list.`);
+            console.error(`There is a domain error in getting user details.`);
             console.error(`We should convert it to a proper http error and also log it.`);
             console.error(exception);
             
